64310af9f84c42af2444ae1add0da0b03082b9b9
Patrick Machalet <s203932@dtu.dk>
2026-03-05 15:00:01 +0100

    Fully working Kalaha implementation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np 
 
-
 # The first 6th represents the 6 pits and the 7th represents the goal
 # of the given player
 player = np.array([6,6,6,6,6,6,0])
@@ -8,12 +7,31 @@ player = np.array([6,6,6,6,6,6,0])
 # The board is the 2 arrays for each player half of the board
 board = np.array([player,player])
 
-def pointsOfPlayer(playerNumber:int, board:np.array):
+# The currentplayer counter 
+currentPlayer:int = 1
+
+# Variable to store extra turn value
+extraTurn:int = 0
+
+def pointsOfPlayer(playerNumber:int, board:np.ndarray):
     return board[playerNumber][6]
 
+def gameOver(board:np.ndarray, currentPlayer: int) -> bool:
 
+    opposingPlayer:int = (currentPlayer+1)%2
 
-def moveRocksFromPitt(playerNumber:int, board:np.array, pittNumber:int) -> int:
+    if np.any(np.all(board[:, :6] == 0, axis=1)):
+
+        remaining = np.sum(board[opposingPlayer][:6])
+
+        board[currentPlayer][6] += remaining
+        board[opposingPlayer][:6] = 0
+
+        return True
+        
+    return False
+
+def moveRocksFromPitt(playerNumber:int, board:np.ndarray, pittNumber:int) -> int:
     """
     Input:\n
     playerNumber: 0 or 1 for selecting the player\n
@@ -30,6 +48,9 @@ def moveRocksFromPitt(playerNumber:int, board:np.array, pittNumber:int) -> int:
         return -1
     
     stones:int = board[playerNumber][pittNumber]
+
+    if stones <= 0:
+        return -1
 
     board[playerNumber][pittNumber] = 0
 
@@ -66,8 +87,8 @@ def moveRocksFromPitt(playerNumber:int, board:np.array, pittNumber:int) -> int:
         opposingPlayer = (playerNumber + 1)%2
 
         #Empty the opposing players pitt into the current players goal
-        board[playerNumber][6] += board[opposingPlayer][currentPitt]
-        board[opposingPlayer][currentPitt] = 0
+        board[playerNumber][6] += board[opposingPlayer][5-currentPitt]
+        board[opposingPlayer][5-currentPitt] = 0
 
 
     # If the last stone ends in the current players goal, then they get an extra turn
@@ -79,15 +100,79 @@ def moveRocksFromPitt(playerNumber:int, board:np.array, pittNumber:int) -> int:
     return 0
 
 
+def printTable(board:np.ndarray, currentPlayer:int, choice:bool):
+
+    # print the pitts of the opposing player in reverse order (don't print the goal)
+    opposingPlayer:int = (currentPlayer+1)%2
+
+    opposingPlayerRow:np.ndarray = board[opposingPlayer][::-1]
+    currentPlayerRow:np.ndarray = board[currentPlayer]
+
+    emptySpace:str =''
+    print('\t\t', end="") 
+
+    for pitt in opposingPlayerRow[1:]:
+        print(f'[ {pitt} ]', end="\t")
+
+    # print the goals of opposing player first - then 6 spaces for the pitts and then current players goal
+    print(f'\nPlayer {opposingPlayer+1} |{opposingPlayerRow[0]}| \t\t\t\t\t\t\t |{currentPlayerRow[6]}| Player {currentPlayer+1}')
 
 
-# Temporary for testing
-tempPlayer1 = np.array([0,6,6,6,6,6,10])
-tempPlayer2 = np.array([7,6,6,6,6,6,0])
+    # print the current player's 6 pitts
 
-# Temporary board for testing
-tempBoard = np.array([tempPlayer1,tempPlayer2])
+    print('\t\t', end= '')
+    for pitt in currentPlayerRow[:6]:   
+        print(f'[ {pitt} ]', end="\t")
+    
 
-print(tempBoard)
-print(f'\nShould they get an extra turn: {moveRocksFromPitt(1,tempBoard,0)}')
-print(tempBoard)
+    # if the current player can currently choose
+    if choice:
+
+        # print arrows in pointing to each pitt of the current player
+        print("\n\t", end='')
+        for i in range(6):
+            print("\t  /\\", end='')
+        # print the numbers for each arrow with "option:" to the far left
+        print('\nOption: ', end='')
+
+        for option in range(1,7):
+            print(f'\t  {option}', end='')
+        print("\n")
+
+
+
+
+while(not gameOver(board, currentPlayer)):
+
+    if(extraTurn == 0):
+        currentPlayer = (currentPlayer+1)%2
+
+    print(f'\nPlayer {currentPlayer+1}\'s turn\n')
+
+    ## I need to print it from the point of view from the current player
+    ## Arrows should show options and names should be next to their goals
+
+    printTable(board=board, currentPlayer=currentPlayer,choice=True)
+    
+    ## I should then let them choose which field they want to move the stones from
+
+    playerChoice:int = int(input("Select what field you'd like to move the stones from:"))-1
+
+    extraTurn = moveRocksFromPitt(playerNumber=currentPlayer,board=board,pittNumber=playerChoice)
+
+    ## Print board state again after the move
+    print('\nBoard after the move\n')
+    printTable(board=board, currentPlayer=currentPlayer,choice=False)
+
+    if(extraTurn == -1):
+        print(f'\n\nThe pitt option {playerChoice+1} is not a valid choice.\n')
+
+    elif(extraTurn == 1):
+        print(f'\n\nPlayer {currentPlayer+1} ended their turn in their goal. They\'re granted an extra turn.\n')
+    
+    else:
+        print('\n\n')
+
+
+print("\nThe game has ended\n")
+print(f'Player 1 won with {board[0][6]} points to player 2\'s {board[1][6]} points') if board[0][6]>board[1][6] else print(f'Player 2 won with {board[1][6]} points to player 1\'s {board[0][6]} points') if board[0][6]<board[1][6] else print(f'The game is tied as Player 1 has {board[0][6]} points and player 2 has {board[1][6]} points')
